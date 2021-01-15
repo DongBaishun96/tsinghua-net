b7dfb291701a4f758304ac14b98382f95c614179
dongbaishun <dongbaishun96@163.com>
2021-01-15 19:57:29 +0800

    update setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'tsinghua_net=tsinghua_net_dongbaishun.tsinghua_net:main'
+            'tsinghua_net=tsinghua_net.tsinghua_net:main'
         ]
     }
 )
