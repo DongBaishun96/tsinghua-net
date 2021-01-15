8b2309b015dc92c265168faf502a0364a04d32cd
dongbaishun <dongbaishun96@163.com>
2021-01-15 19:58:35 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="tsinghua_net_dongbaishun",
+    name="tsinghua_net",
     version="1.0.0",
     description="Tsinghua network tools",
     long_description=long_description,
